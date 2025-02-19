94d1a21ecfc6eae3408f1a4470129a31b78b08a5
Thilan Hasitha <156594121+thilanhasitha@users.noreply.github.com>
2025-02-19 13:12:26 +0530

    route and page strucrture

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,30 @@
 import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Reports from './pages/Reports'
+import Dashboard from './pages/Dashboard'
+import Electronicitems from './pages/Items/Electronicitems'
+import Householditems from './pages/Items/Householditems'
+import Groceryitems from './pages/Items/Groceryitems'
+import Items from './pages/Items/items'
+
+
 
 export default function App() {
   return (
-    <div>
-      App
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/'element={<Home/>}></Route>
+        <Route path='/reports'element={<Reports/>}></Route>
+        <Route path='/dashboard'element={<Dashboard/>}></Route>
+        <Route path='/items'element={<Items/>}></Route>
+        <Route path="/grocery"element={<Groceryitems/>}></Route>
+        <Route path='/electronic'element={<Electronicitems/>}></Route>
+        <Route path='/household'element={<Householditems/>}></Route>
+        
+
+
+      </Routes>
+    </BrowserRouter>
   )
 }
---
+++ client/src/pages/Items/Groceryitems.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Groceryitems() {
+  return (
+    <div>
+        grocery items
+      
+    </div>
+  )
+}
