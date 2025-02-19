bce811714a3ab22e4370cd1682a1cc98245c21f2
Thilan Hasitha <156594121+thilanhasitha@users.noreply.github.com>
2025-02-19 17:49:55 +0530

    connect the database

--- api/index.js
+++ api/index.js
@@ -1,6 +1,18 @@
 import express from 'express';
+import mongoose from 'mongoose'; 
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+mongoose.connect(
+    process.env.MONGO
+ )
+    .then(() => console.log("Connected to MongoDB"))
+    .catch(err => console.error("MongoDB connection error:", err));
 
 const app = express();
-app.listen(3000 ,()=>{
-    console.log("server is running port 3000!!");
-})
+
+
+app.listen(3000, () => {
+    console.log("Server is running on port 3000!!");
+});
