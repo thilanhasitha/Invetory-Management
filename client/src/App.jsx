a5dbcbbc5b9d8aad4587c119c8d9aac9303723be
Thilan <yasanjithbgth@gmail.com>
2025-03-03 12:59:47 +0530

    install react js and tailwindcss

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import "tailwindcss";
+
+export default function App() {
+  return (
+    <div>
+      <h1 className='text-3xl text-red-500'> 
+      Hello Inventory</h1>
+    </div>
+  )
+}
